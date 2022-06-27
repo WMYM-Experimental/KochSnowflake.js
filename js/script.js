af213a35fddc23060e971ce80eb48d1b3b4b9cba
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-26 23:06:44 -0500

    geometry and trigonometry used for draw the koch curve

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.strokeStyle = "white";
 
 let size = Math.ceil(canvas.width / 2.5);
 
@@ -14,6 +15,7 @@ class Point {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, size / 2, 0, Math.PI * 2);
+        ctx.stroke();
         ctx.fill();
     }
 }
@@ -25,4 +27,35 @@ const p2 = new Point(
     p0.y - Math.sqrt(Math.pow(size, 2) - Math.pow(size / 2, 2))
 );
 
-console.log(p0, p1, p2);
+const kochCurve = (p0, p1) => {
+    let distance = Math.sqrt(
+        Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2)
+    );
+
+    let div = Math.floor(distance / 3);
+    let angle = Math.atan2(p1.y - p0.y, p1.x - p0.x);
+
+    let pA = new Point(p0.x + (p1.x - p0.x) / 3, p0.y + (p1.y - p0.y) / 3);
+
+    let pB = new Point(
+        pA.x + div * Math.cos(angle + Math.PI / 3),
+        pA.y + div * Math.sin(angle + Math.PI / 3)
+    );
+
+    let pC = new Point(p1.x - (p1.x - p0.x) / 3, p1.y - (p1.y - p0.y) / 3);
+
+    ctx.strokeStyle = "#006d77";
+
+    ctx.beginPath();
+    ctx.moveTo(p0.x, p0.y);
+
+    ctx.lineTo(pA.x, pA.y);
+    ctx.lineTo(pB.x, pB.y);
+    ctx.lineTo(pC.x, pC.y);
+
+    ctx.lineTo(p1.x, p1.y);
+    ctx.stroke();
+    ctx.closePath();
+};
+
+kochCurve(p0, p1);
